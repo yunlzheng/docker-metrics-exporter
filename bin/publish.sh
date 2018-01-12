2baa7a1c88bd94b4075d81763cbb5688bbf8e420
yunl.zheng <yunl.zheng@gmail.com>
2018-01-12 16:02:52 +0800

    cal container cpu usage

--- bin/publish.sh
+++
@@ -1,3 +0,0 @@
-./gradlew build
-docker build -t registry.cn-hangzhou.aliyuncs.com/wise2c-test/docker-metrics-exporter:stable -f Dockerfile-local .
-docker push registry.cn-hangzhou.aliyuncs.com/wise2c-test/docker-metrics-exporter:stable
--- src/main/java/org/prometheus/exporter/exporter/ContainerMetricsCollector.java
+++ src/main/java/org/prometheus/exporter/exporter/ContainerMetricsCollector.java
@@ -11,6 +11,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
+/**
+ * How to cal container metrics https://github.com/moby/moby/blob/eb131c5383db8cac633919f82abad86c99bffbe5/cli/command/container/stats_helpers.go#L185:75
+ */
 public class ContainerMetricsCollector implements Runnable {
 
     private static Logger LOGGER = LoggerFactory.getLogger(ContainerMetricsCollector.class);
@@ -38,16 +41,24 @@ public class ContainerMetricsCollector implements Runnable {
                 labels.put(String.format("container_label_%s", key.replace(".", "_")).toLowerCase(), containerLabels.get(key));
             }
             String containerName = getContainerName(container);
-            LOGGER.info("inspect start container " + containerName + " information");
             labels.put("name", containerName);
             labels.put("image", container.image());
             ContainerStats stats = docker.stats(container.id());
-            Long limit = stats.memoryStats().limit();
-            Long used = stats.memoryStats().usage();
-            double usage = new BigDecimal(Double.toString(used)).divide(new BigDecimal(Double.toString(limit)), 4, BigDecimal.ROUND_HALF_UP).doubleValue();
-            metrics = new ContainerMetrics(limit, used, usage);
+            Long memLimit = stats.memoryStats().limit();
+            Long memUsed = stats.memoryStats().usage();
+
+            Long previousCPU = stats.precpuStats().cpuUsage().totalUsage();
+            Long previousSystem = stats.precpuStats().systemCpuUsage();
+            double cpuPercent = calculateCPUPercentUnix(previousCPU, previousSystem, stats);
+
+            double memUsage = new BigDecimal(Double.toString(memUsed))
+                    .divide(
+                            new BigDecimal(Double.toString(memLimit)), 4, BigDecimal.ROUND_HALF_UP
+                    )
+                    .doubleValue();
+            metrics = new ContainerMetrics(memLimit, memUsed, memUsage, cpuPercent);
+            LOGGER.info(metrics.toString());
             collected = true;
-            LOGGER.info("inspect done container " + containerName + " information");
 
         } catch (Exception e) {
             collected = false;
@@ -58,6 +69,29 @@ public class ContainerMetricsCollector implements Runnable {
             }
         }
 
+    }
+
+    private double calculateCPUPercentUnix(Long previousCPU, Long previousSystem, ContainerStats stats) {
+        double cpuPercent = 0.0d;
+        long cpuDelta = stats.cpuStats().cpuUsage().totalUsage() - previousCPU;
+        long systemDelta = stats.cpuStats().systemCpuUsage() - previousSystem;
+
+        if (systemDelta > 0 && cpuDelta > 0) {
+            cpuPercent = getCpuUsage(cpuDelta, systemDelta) * stats.cpuStats().cpuUsage().percpuUsage().size() * 100.0d;
+        }
+        return cpuPercent;
+
+    }
+
+    private double getCpuUsage(long cpuDelta, long systemDelta) {
+
+
+        double usage = new BigDecimal(Double.toString(cpuDelta))
+                .divide(
+                        new BigDecimal(Double.toString(systemDelta)), 4, BigDecimal.ROUND_HALF_UP
+                ).doubleValue();
+
+        return usage;
     }
 
     private String getContainerName(Container container) {
@@ -81,12 +115,14 @@ public class ContainerMetricsCollector implements Runnable {
 class ContainerMetrics {
     private long memLimit;
     private long memUsed;
+    private double cpuPercent;
     private double memUsage;
 
-    public ContainerMetrics(long memLimit, long memUsed, double memUsage) {
+    public ContainerMetrics(long memLimit, long memUsed, double memUsage, double cpuPercent) {
         this.memLimit = memLimit;
         this.memUsage = memUsage;
         this.memUsed = memUsed;
+        this.cpuPercent = cpuPercent;
     }
 
     public long getMemLimit() {
@@ -111,5 +147,15 @@ class ContainerMetrics {
 
     public void setMemUsed(long memUsed) {
         this.memUsed = memUsed;
+    }
+
+    @Override
+    public String toString() {
+        return "ContainerMetrics{" +
+                "cpuPercent=" + cpuPercent +
+                ", memLimit=" + memLimit +
+                ", memUsed=" + memUsed +
+                ", memUsage=" + memUsage +
+                '}';
     }
 }
